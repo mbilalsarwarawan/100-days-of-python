f5b844a0e1c454147402dabe0b36c9be4765957b
mbilalsarwarawan <mbilalsarwarawan@gmail.com>
2023-11-21 10:33:48 +0500

    day36

--- day35/core/home/models.py
+++ day35/core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    #id =models.AutoField()
+    name = models.CharField(max_length=100)
+    age= models.IntegerField()
+    email=models.EmailField()
+    address= models.TextField()
+
+class Car(models.Model):
+    car_name=models.CharField(max_length=100)
+    car_speed=models.IntegerField(default=50)
+
--- day35/core/home/views.py
+++ day35/core/home/views.py
@@ -4,6 +4,16 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def home(request):
+    fruits = [
+        {"fruit": "apple", "color": "red"},
+        {"fruit": "banana", "color": "yellow"},
+        {"fruit": "grape", "color": "purple"},
+        {"fruit": "orange", "color": "orange"},
+        {"fruit": "watermelon", "color": "green"}
+    ]
+    return render(request, "pages/success-story.html",context={'fruits' : fruits})
+
 
 def main(request):
     return HttpResponse("""<h1>Hello my first django app</h1>""")
